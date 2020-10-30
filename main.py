879af1d0756ad3eee3e56bee7cfcd6ebf6662b81
OlegKlochenok <ok@roundabout.ru>
2020-10-30 18:45:11 +0300

    добавление учебных файлов сайта

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
